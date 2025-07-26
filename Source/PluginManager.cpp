2ae0f65159c303751c0d7c75e7879a5f8c78261f
Ruchir Shah <@ruchirlives>
2025-07-26 20:20:41 +0100

    Revert "Fix sample rate handling"
    This reverts commit e61616a3dbc458d0d855b81649d66965d5d7c7ce.

--- Source/PluginManager.cpp
+++ Source/PluginManager.cpp
@@ -67,12 +67,11 @@ PluginManager::~PluginManager()
 
 void PluginManager::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
-    currentSampleRate = sampleRate;
     for (auto& [pluginId, pluginInstance] : pluginInstances)
     {
         if (pluginInstance != nullptr)
         {
-                        pluginInstance->prepareToPlay(sampleRate, samplesPerBlockExpected);
+			pluginInstance->prepareToPlay(sampleRate, samplesPerBlockExpected);
 
         }
     }
